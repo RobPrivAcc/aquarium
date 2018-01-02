1d091270aeefbd8ce5e09c8bd90db76c035b2e3c
Robert <art@republika.pl>
2018-01-02 13:16:41 +0000

    Initial commit

---
+++ aquarium.ino
@@ -0,0 +1,46 @@
+  /******  VARIABLES  *****/
+  const int pumpMain = 0;
+  const int pumpAlt = 0;
+  const int lampMain1 = 0;
+  const int lampMain2 = 0;
+  const int CO2 = 0;
+  const int feedingBtn = 0;
+  const int feedingTime = 5;
+  
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pumpMain, OUTPUT);
+  pinMode(pumpAlt, OUTPUT);
+  pinMode(lampMain1, OUTPUT);
+  pinMode(lampMain2, OUTPUT);
+  pinMode(CO2, OUTPUT);
+  pinMode(feedingBtn, INPUT);
+}
+
+void morningStart(){
+  digitalWrite(lampMain1, HIGH);
+  digitalWrite(lampMain2, HIGH);
+  digitalWrite(CO2, HIGH);
+  }
+
+void evningStop(){
+  digitalWrite(lampMain1, LOW);
+  digitalWrite(lampMain2, LOW);
+  digitalWrite(CO2, LOW);
+  }
+
+void feedingStart(){
+  digitalWrite(pumpMain, LOW);
+  digitalWrite(pumpAlt, LOW);
+  }
+
+void feedingEnd(){
+  digitalWrite(pumpMain, HIGH);
+  digitalWrite(pumpAlt, HIGH);
+  }  
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
